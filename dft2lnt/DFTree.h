43e4245ae7a2f18a33b1f252324b38fcb5eb6e50
Freark van der Berg <fivanderberg@gmail.com>
2011-09-28 12:18:13 +0200

    Added DFTree::getTopNode()

--- dft2lnt/DFTree.h
+++ dft2lnt/DFTree.h
@@ -20,50 +20,52 @@ private:
 	std::vector<Nodes::Node*> nodes;
 	std::map<std::string,DFT::Nodes::Node*> nodeTable;
 	Nodes::Node* topNode;
-	
+
 	int setTopNode_(Nodes::Node* node) {
 		this->topNode = node;
 	}
-	
+
 public:
 	DFTree(): nodes(0), topNode(NULL) {
-		
+
 	}
 	virtual ~DFTree() {
-		for(int i=nodes.size();i--;) {
+		for(int i=nodes.size(); i--;) {
 			assert(nodes.at(i));
 			delete nodes.at(i);
 		}
 	}
-	
+
 	// Claims ownership
 	void addNode(Nodes::Node* node) {
 		//std::cout << "Added node: " << node->getName() << ", type: " << node->getType() << std::endl;
 		nodes.push_back(node);
 	}
-	
+
 	Nodes::Node* getNode(const std::string& name) {
-		for(int i=0; i<nodes.size();++i) {
+		for(int i=0; i<nodes.size(); ++i) {
 			if(nodes.at(i)->getName()==name) {
 				return nodes.at(i);
 			}
 		}
 	}
-	
+
 	std::vector<Nodes::Node*>& getNodes() {
 		return nodes;
 	}
-	
+
 	int setTopNode(Nodes::Node* node) {
 		// Check if the specified node is in this DFT
-		for(int i=0; i<nodes.size();++i) {
+		for(int i=0; i<nodes.size(); ++i) {
 			if(nodes.at(i)==node) {
 				return setTopNode_(node);
 			}
 		}
 		return 1; // FIXME: ERROR HANDLING
 	}
-	
+	Nodes::Node* getTopNode() {
+		return topNode;
+	}
 };
 } // Namespace: DFT
 
