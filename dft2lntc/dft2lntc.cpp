b2c2bb499faf2468805c2e652451c29930cde180
Freark van der Berg <fivanderberg@gmail.com>
2011-10-28 17:17:12 +0200

    Compiler: added -a, -t, -o, -s, -x, -b, --color; now using new testWritable()

--- dft2lntc/dft2lntc.cpp
+++ dft2lntc/dft2lntc.cpp
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <stdarg.h>
 #include <string>
+#include <fstream>
 #include <libgen.h>
 #include <getopt.h>
 
@@ -16,15 +17,25 @@
 #include "DFTreeValidator.h"
 #include "DFTreePrinter.h"
 #include "DFTreeSVLAndLNTBuilder.h"
+#include "DFTreeEXPBuilder.h"
 
 FILE* pp_outputFile = stdout;
 
 void print_help() {
 	printf("dft2lnt [INPUTFILE.dft] [options]\n");
 	printf(
-"Compiles the inputfile to Lotos NT files and SVL script. If no inputfile was specified, stdin is used.\n"
+"Compiles the inputfile to EXP and SVL script. If no inputfile was specified, stdin is used.\n"
+"If no outputfile was specified, 'a.svl' and 'a.exp' are used.\n"
 "\n"
 "Options:\n"
+"  -a FILE     Output AST to file. '-' for stdout.\n"
+"  -t FILE     Output DFT to file. '-' for stdout.\n"
+"  -h, --help  Show this help.\n"
+"  -o FILE     Output EXP to <FILE>.exp and SVL to <FILE>.svl.\n"
+"  -s FILE     Output EXP to file. '-' for stdout. Overrules -o.\n"
+"  -x FILE     Output SVL to file. '-' for stdout. Overrules -o.\n"
+"  -b FILE     Output of SVL to this BCG file.\n"
+"  --color     Use colored messages.\n"
 	);
 }
 
@@ -33,22 +44,27 @@ int main(int argc, char** argv) {
 	/* Command line arguments and their default settings */
 	string inputFileName     = "";
 	int    inputFileSet      = 0;
-	string outputFileName    = "a.out";
+	string outputFileName    = "a";
 	int    outputFileSet     = 0;
 	string outputASTFileName = "";
 	int    outputASTFileSet  = 0;
-	string outputASMFileName = "";
-	int    outputASMFileSet  = 0;
-	string outputRUNFileName = "";
-	int    outputRUNFileSet  = 0;
+	string outputDFTFileName = "";
+	int    outputDFTFileSet  = 0;
+	string outputSVLFileName = "";
+	int    outputSVLFileSet  = 0;
+	string outputEXPFileName = "";
+	int    outputEXPFileSet  = 0;
+	string outputBCGFileName = "";
+	int    outputBCGFileSet  = 0;
 
-	int         optimize          = 1;
-	int         execute           = 0;
-	int			stopAfterPreproc  = 0;
+	int         optimize           = 1;
+	int         execute            = 0;
+	int			stopAfterPreproc   = 0;
+	int			useColoredMessages = 0;
 
 	/* Parse command line arguments */
 	char c;
-	while( (c = getopt(argc,argv,"o:a:s:gEr:h-:")) >= 0 ) {
+	while( (c = getopt(argc,argv,"o:a:t:s:x:h-:")) >= 0 ) {
 		switch(c) {
 
 			// -o FILE
@@ -57,16 +73,8 @@ int main(int argc, char** argv) {
 					outputFileName = "";
 					outputFileSet = 1;
 				} else {
-					printf("-o: '%s'\n",optarg);
 					outputFileName = string(optarg);
 					outputFileSet = 1;
-					FILE* f = fopen(optarg,"a");
-					if(f) {
-						fclose(f);
-					} else {
-						fprintf(stderr,"unable to open outputfile %s\n",optarg);
-						return 1;
-					}
 				}
 				break;
 
@@ -75,67 +83,53 @@ int main(int argc, char** argv) {
 				if(strlen(optarg)==1 && optarg[0]=='-') {
 					outputASTFileName = "";
 					outputASTFileSet = 1;
-					printf("outputASTFileSet");
 				} else {
 					outputASTFileName = string(optarg);
 					outputASTFileSet = 1;
-					FILE* f = fopen(optarg,"a");
-					if(f) {
-						fclose(f);
-					} else {
-						fprintf(stderr,"unable to open outputASTfile %s\n",optarg);
-						return 1;
-					}
+				}
+				break;
+
+			// -t FILE
+			case 't':
+				if(strlen(optarg)==1 && optarg[0]=='-') {
+					outputDFTFileName = "";
+					outputDFTFileSet = 1;
+				} else {
+					outputDFTFileName = string(optarg);
+					outputDFTFileSet = 1;
 				}
 				break;
 
 			// -s FILE
 			case 's':
 				if(strlen(optarg)==1 && optarg[0]=='-') {
-					outputASMFileName = "";
-					outputASMFileSet = 1;
+					outputSVLFileName = "";
+					outputSVLFileSet = 1;
 				} else {
-					outputASMFileName = string(optarg);
-					outputASMFileSet = 1;
-					FILE* f = fopen(optarg,"a");
-					if(f) {
-						fclose(f);
-					} else {
-						fprintf(stderr,"unable to open outputASMfile %s\n",optarg);
-						return 1;
-					}
+					outputSVLFileName = string(optarg);
+					outputSVLFileSet = 1;
 				}
 				break;
 
-
-			// -g
-			case 'g':
-				optimize = 0;
+			// -x FILE
+			case 'x':
+				if(strlen(optarg)==1 && optarg[0]=='-') {
+					outputEXPFileName = "";
+					outputEXPFileSet = 1;
+				} else {
+					outputEXPFileName = string(optarg);
+					outputEXPFileSet = 1;
+				}
 				break;
 
-			// -E
-			case 'E':
-				stopAfterPreproc = 1;
-				break;
-
-			// -r
-			case 'r':
-				execute = 1;
-				if(optarg) {
-					if(strlen(optarg)==1 && optarg[0]=='-') {
-						outputRUNFileName = "";
-						outputRUNFileSet = 1;
-					} else {
-						outputRUNFileName = string(optarg);
-						outputRUNFileSet = 1;
-						FILE* f = fopen(optarg,"a");
-						if(f) {
-							fclose(f);
-						} else {
-							fprintf(stderr,"unable to open input file for execution %s\n",optarg);
-							return 1;
-						}
-					}
+			// -b FILE
+			case 'b':
+				if(strlen(optarg)==1 && optarg[0]=='-') {
+					outputBCGFileName = "";
+					outputBCGFileSet = 1;
+				} else {
+					outputBCGFileName = string(optarg);
+					outputBCGFileSet = 1;
 				}
 				break;
 
@@ -146,9 +140,13 @@ int main(int argc, char** argv) {
 
 			// --
 			case '-':
-				if(!strcmp(optarg,"help")) {
+				if(!strcmp("help",optarg)) {
 					print_help();
 					exit(0);
+				} else if(!strcmp("color",optarg)) {
+					useColoredMessages = true;
+				} else if(!strcmp("no-color",optarg)) {
+					useColoredMessages = false;
 				}
 		}
 	}
@@ -158,6 +156,10 @@ int main(int argc, char** argv) {
 //		printf("  %s\n",argv[i]);
 //	}
 
+	/* Create a new compiler context */
+	DFT::CompilerContext* compilerContext = new DFT::CompilerContext(std::cerr);
+	compilerContext->useColoredMessages(useColoredMessages);
+
 	/* Parse command line arguments without a -X.
 	 * These specify the input files. Currently it will only allow
 	 * one input file.
@@ -166,7 +168,7 @@ int main(int argc, char** argv) {
 		int isSet = 0;
 		for(unsigned int i=optind; i<(unsigned int)argc; ++i) {
 			if(isSet) {
-				fprintf(stderr,"too many input files: %s\n",argv[i]);
+				compilerContext->reportError("too many input files: "+string(argv[i]));
 				continue;
 			}
 			isSet = 1;
@@ -179,10 +181,30 @@ int main(int argc, char** argv) {
 				if(f) {
 					fclose(f);
 				} else {
-					fprintf(stderr,"unable to open inputfile %s\n",argv[i]);
+					compilerContext->reportError("unable to open inputfile " + string(argv[i]));
 					return 1;
 				}
 			}
+		}
+	}
+	
+	/*
+	 * Handle arguments
+	 */
+	{
+		// Set SVL and EXP output file names
+		outputSVLFileName = outputSVLFileSet ? outputSVLFileName : outputFileName + "." + DFT::FileExtensions::SVL;
+		outputEXPFileName = outputEXPFileSet ? outputEXPFileName : outputFileName + "." + DFT::FileExtensions::EXP;
+		outputBCGFileName = outputBCGFileSet ? outputBCGFileName : outputFileName + "." + DFT::FileExtensions::BCG;
+		outputFileSet = outputFileSet || outputSVLFileSet || outputEXPFileSet;
+		
+		// Test all the files that need to be written if they are writable
+		bool ok = true;
+		ok = outputSVLFileName=="" ? ok : compilerContext->testWritable(outputSVLFileName) ? ok : false;
+		ok = outputEXPFileName=="" ? ok : compilerContext->testWritable(outputEXPFileName) ? ok : false;
+		ok = outputDFTFileName=="" ? ok : compilerContext->testWritable(outputDFTFileName) ? ok : false;
+		ok = outputASTFileName=="" ? ok : compilerContext->testWritable(outputASTFileName) ? ok : false;
+		if(!ok) {
 		}
 	}
 
@@ -239,13 +261,11 @@ int main(int argc, char** argv) {
 		putchar(c);
 	}
 */
-	if(inputFileName.empty()) {
-		printf("No inputfile specified. See codolc --help\n");
-		return 0;
-	}
-
-	/* Create a new compiler context */
-	DFT::CompilerContext* compilerContext = new DFT::CompilerContext(inputFileName);
+//	if(inputFileName.empty()) {
+//		printf("No inputfile specified. See dft2lntc --help\n");
+//		return 0;
+//	}
+//
 	
 	char* real_path = new char[PATH_MAX];
 	cwd_realpath(inputFileName.c_str(),real_path);
@@ -255,15 +275,16 @@ int main(int argc, char** argv) {
 	std::string parserInputFileName(path_basename(inputFileName.c_str()));
 	
 	/* Parse input file */
-	printf(":: Checking syntax...\n"); fflush(stdout);
+	compilerContext->message("Checking syntax...");
 	Parser* parser = new Parser(inputFile,parserInputFilePath,compilerContext);
 	std::vector<DFT::AST::ASTNode*>* ast = parser->parse();
 	if(!ast) {
-		printf(":error:Could not build AST\n");
+		compilerContext->reportError("could not build AST");
 		return 1;
 	}
+
 	/* Validate input */
-	printf(":: Validating input...\n"); fflush(stdout);
+	compilerContext->message("Validating input...");
 	int astValid = false;
 	{
 		DFT::ASTValidator validator(ast,compilerContext);
@@ -274,10 +295,21 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 	
+	/* Print AST */
+	if(ast && outputASTFileSet) {
+//		DFT::ASTPrinter printer(ast,compilerContext);
+//		if(outputASTFileName!="") {
+//			std::ofstream astFile (outputASTFileName);
+//			printer.print(astFile);
+//		} else {
+//			printer.print(std::cout);
+//		}
+	}
+	
 	/* Create DFT */
 	DFT::DFTree* dft = NULL;
 	if(astValid) {
-		printf(":: Building DFT...\n"); fflush(stdout);
+		compilerContext->message("Building DFT...");
 		DFT::ASTDFTBuilder builder(ast,compilerContext);
 		dft = builder.build();
 	}
@@ -289,29 +321,57 @@ int main(int argc, char** argv) {
 	/* Validate DFT */
 	int dftValid = false;
 	if(dft) {
-		printf(":: Validating DFT...\n"); fflush(stdout);
+		compilerContext->message("Validating DFT...");
 		DFT::DFTreeValidator validator(dft,compilerContext);
 		dftValid = validator.validate();
-		if(dftValid) {
-			printf(":: DFT determined valid\n");
-		} else {
+		if(!dftValid) {
 			printf(":: DFT determined invalid\n");
+//		} else {
+//			printf(":: DFT determined valid\n");
 		}
 	}
 	
 	/* Printing DFT */
-	if(dft) {
-		printf(":: Printing DFT...\n"); fflush(stdout);
+	if(dft && outputDFTFileSet) {
+		compilerContext->message("Printing DFT...");
 		DFT::DFTreePrinter printer(dft,compilerContext);
-		printer.print(std::cout);
+		if(outputFileName!="") {
+			std::ofstream dftFile (outputDFTFileName);
+			printer.print(dftFile);
+		} else {
+			printer.print(std::cout);
+		}
+		
 	}
 
 	/* Building SVL and LNT out of DFT */
-	if(dft) {
-		printf(":: Building SVL and LNT...\n"); fflush(stdout);
-		DFT::DFTreeSVLAndLNTBuilder builder(".","try",dft,compilerContext);
+//	if(dft) {
+//		compilerContext->message("Building SVL and LNT...");
+//		DFT::DFTreeSVLAndLNTBuilder builder("/home/studfmg/bergfi/dft2lnt/dft2lnt/build/Content",".","try",dft,compilerContext);
+//		builder.build();
+//	}
+
+	/* Building EXP out of DFT */
+	if(dft && outputFileSet) {
+		compilerContext->message("Building EXP...");
+		DFT::DFTreeEXPBuilder builder("/home/studfmg/bergfi/dft2lnt/dft2lnt/build/Content",".",outputBCGFileName,outputEXPFileName,dft,compilerContext);
 		builder.build();
+		
+		if(outputSVLFileName!="") {
+			std::ofstream svlFile (outputSVLFileName);
+			builder.printSVL(svlFile);
+		} else {
+			builder.printSVL(std::cout);
+		}
+		if(outputEXPFileName!="") {
+			std::ofstream expFile (outputEXPFileName);
+			builder.printEXP(expFile);
+		} else {
+			builder.printEXP(std::cout);
+		}
+		
 	}
+
 	delete dft;
 	
 }
