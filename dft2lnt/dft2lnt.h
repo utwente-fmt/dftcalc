f167dbcc34b4b5237b7af525378e05cb88b72fff
Freark van der Berg <fivanderberg@gmail.com>
2012-01-12 16:05:59 +0100

    Removed unused code from dft2lnt.h

--- dft2lnt/dft2lnt.h
+++ dft2lnt/dft2lnt.h
@@ -7,29 +7,11 @@
 #include "compiler.h"
 #include "mrmc.h"
 
-namespace DFT {
-
 class DFT2LNT {
 public:
-	static int validateCommands(const std::vector<DFT::AST::ASTNode*>* ast) {
-		int valid = true;
-		for(size_t i=0; i<ast->size(); ++i) {
-//			valid = ast->at(i)->validate() ? valid : false;
-		}
-		return valid;
-	}
-	static DFTree* createDFT(const std::vector<DFT::AST::ASTNode*>* ast) {
-		DFTree* dft = new DFTree();
-		for(size_t i=0; i<ast->size(); ++i) {
-//			ast->at(i)->handleCommand(dft);
-		}
-		return dft;
-	}
 	static const std::string LNTSUBROOT;
 	static const std::string BCGSUBROOT;
 
 };
-
-} //Namespace: DFT
 
 #endif // DFT2LNT_H
