081be4a6f49d1887c1961db0a3f9d082fb677f0a
Freark van der Berg <fivanderberg@gmail.com>
2012-01-12 16:03:29 +0100

    CompilerContext: now makes use of the new MessageFormatter

--- dft2lnt/compiler.cpp
+++ dft2lnt/compiler.cpp
@@ -2,167 +2,17 @@
 
 #include "compiler.h"
 
-const CompilerContext::MessageType CompilerContext::MessageType::Message(MessageType::MESSAGE);
-const CompilerContext::MessageType CompilerContext::MessageType::Notify (MessageType::NOTIFY);
-const CompilerContext::MessageType CompilerContext::MessageType::Action (MessageType::ACTION);
-const CompilerContext::MessageType CompilerContext::MessageType::Warning(MessageType::WARNING);
-const CompilerContext::MessageType CompilerContext::MessageType::Success(MessageType::SUCCESS);
-const CompilerContext::MessageType CompilerContext::MessageType::Error  (MessageType::ERR);
-const CompilerContext::MessageType CompilerContext::MessageType::File   (MessageType::FILE);
-const CompilerContext::MessageType CompilerContext::MessageType::Title  (MessageType::TITLE);
 
-const int CompilerContext::VERBOSITY_DEFAULT = 0;
 
-void CompilerContext::print(const Location& loc, const std::string& str, const MessageType& mType) {
 
-	if(mType.isError()) {
-		consoleWriter << ConsoleWriter::Color::Error;
-	} else if (mType.isWarning()) {
-		consoleWriter << ConsoleWriter::Color::Warning;
-	} else if (mType.isNotify()) {
-		consoleWriter << ConsoleWriter::Color::Notify;
-	} else if (mType.isAction() || mType.isTitle()) {
-		consoleWriter << ConsoleWriter::Color::Action;
-	} else if (mType.isSuccess()) {
-		consoleWriter << ConsoleWriter::Color::Proper;
-	} else if (mType.isFile()) {
-	} else {
-		consoleWriter << ConsoleWriter::Color::Message;
+
 	}
 
-//	consoleWriter << consoleWriter.applyprefix;
-//	consoleWriter << loc.filename;
-
-	loc.print(consoleWriter.ss());
-
-	if(mType.isError()) {
-		consoleWriter << ":error:";
-	} else if (mType.isWarning()) {
-		consoleWriter << ":warning:";
-	} else if (mType.isNotify()) {
-		consoleWriter << ":: ";
-		consoleWriter << ConsoleWriter::Color::Notify2;
-	} else if (mType.isAction() || mType.isTitle()) {
-		consoleWriter << " > ";
-		consoleWriter << ConsoleWriter::Color::Notify2;
-	} else if (mType.isMessage()) {
-	} else if (mType.isSuccess()) {
-		consoleWriter << " o ";
-		consoleWriter << ConsoleWriter::Color::Reset;
-	} else if (mType.isFile()) {
-	} else {
-		consoleWriter << ":";
-	}
-
-	consoleWriter << str;
-
-	consoleWriter << ConsoleWriter::Color::Reset;
-
-	if (mType.isTitle()) {
-		consoleWriter << ":";
-	}
-	
-	consoleWriter << consoleWriter.applypostfix;
-
-}
 
 
-void CompilerContext::reportErrorAt(Location loc, std::string str, const int& verbosityLevel) {
-	messageAt(loc,str,MessageType::Error,verbosityLevel);
-	errors++;
-}
-
-void CompilerContext::reportWarningAt(Location loc, std::string str, const int& verbosityLevel) {
-	messageAt(loc,str,MessageType::Warning,verbosityLevel);
-	warnings++;
-}
-
-void CompilerContext::reportActionAt(Location loc, std::string str, const int& verbosityLevel) {
-	messageAt(loc,str,MessageType::Action,verbosityLevel);
-}
-
-void CompilerContext::reportError(std::string str, const int& verbosityLevel) {
-	message(str,MessageType::Error,verbosityLevel);
-	errors++;
-}
-
-void CompilerContext::reportWarning(std::string str, const int& verbosityLevel) {
-	message(str,MessageType::Warning,verbosityLevel);
-	warnings++;
-}
-
-void CompilerContext::reportAction(std::string str, const int& verbosityLevel) {
-	message(str,MessageType::Action,verbosityLevel);
-}
-
-void CompilerContext::reportFile(std::string fileName, std::string contents, const int& verbosityLevel) {
-	message(fileName,MessageType::Title,verbosityLevel);
-	message(contents,MessageType::File,verbosityLevel);
-}
-
-void CompilerContext::reportSuccess(std::string  str, const int& verbosityLevel) {
-	message(str,MessageType::Success,verbosityLevel);
-}
-
-void CompilerContext::notify(std::string str, const int& verbosityLevel) {
-	message(str,MessageType::Notify,verbosityLevel);
-}
-
-void CompilerContext::message(std::string str, const int& verbosityLevel) {
-	message(str,MessageType::Message,verbosityLevel);
-}
-
-void CompilerContext::message(std::string str, const MessageType& mType, const int& verbosityLevel) {
-	messageAt(Location(),str,mType,verbosityLevel);
-}
 
 
-void CompilerContext::messageAt(Location loc, std::string str, const MessageType& mType, const int& verbosityLevel) {
-	static int n=1;
 
-	if(verbosityLevel>verbosity) {
-		return;
-	}
-
-//	print(loc,"ADDED: " + str, mType);
-		messages.insert(CompilerContext::MSG(n++,loc,str,mType));
-}
-
-void CompilerContext::reportErrors(const int& verbosityLevel) {
-	if(verbosityLevel>verbosity) {
-		return;
-	}
-
-	consoleWriter << consoleWriter.applyprefix;
-	consoleWriter << ConsoleWriter::Color::Notify << ":: ";
-	consoleWriter << ConsoleWriter::Color::Notify2  << "Finished. ";
-	{
-		if(errors==0) {
-			consoleWriter << ConsoleWriter::Color::Proper;
-		} else {
-			consoleWriter << ConsoleWriter::Color::Error;
-		}
-		consoleWriter << errors << " errors";
-	}
-	consoleWriter << ConsoleWriter::Color::Notify2  << " and ";
-	{
-		if(warnings==0) {
-			consoleWriter << ConsoleWriter::Color::Proper;
-		} else {
-			consoleWriter << ConsoleWriter::Color::Warning;
-		}
-		consoleWriter << warnings << " warnings";
-	}
-	consoleWriter << ConsoleWriter::Color::Notify2 << "." << consoleWriter.applypostfix;
-	consoleWriter << ConsoleWriter::Color::Reset;
-}
-
-void CompilerContext::flush() {
-	std::set<MSG>::iterator it = messages.begin();
-	for(;it!=messages.end(); ++it) {
-		print(it->loc,it->message,it->type);
-	}
-	messages.clear();
 }
 
 bool CompilerContext::testWritable(std::string fileName) {
--- dft2lnt/compiler.h
+++ dft2lnt/compiler.h
@@ -11,6 +11,7 @@
 #include <map>
 #include "ConsoleWriter.h"
 #include "dft_parser_location.h"
+#include "MessageFormatter.h"
 
 /**
  * Every file is associated with an instance of this struct
@@ -35,101 +36,9 @@ typedef struct FileContext {
  * The compiler context contains settings and data related to the compilation
  * of a file and its dependencies.
  */
-class CompilerContext {
-public:
-	class MessageType {
-	public:
-		enum MType {
-
-			MESSAGE=1,
-			NOTIFY,
-			ACTION,
-			WARNING,
-			ERR,
-			SUCCESS,
-			FILE,
-			TITLE,
-
-			MESSAGE_FIRST = MESSAGE,
-			MESSAGE_LAST  = MESSAGE,
-			NOTIFY_FIRST  = NOTIFY,
-			NOTIFY_LAST   = NOTIFY,
-			ACTION_FIRST  = ACTION,
-			ACTION_LAST   = ACTION,
-			WARNING_FIRST = WARNING,
-			WARNING_LAST  = WARNING,
-			ERROR_FIRST   = ERR,
-			ERROR_LAST    = ERR,
-			SUCCESS_FIRST = SUCCESS,
-			SUCCESS_LAST  = SUCCESS,
-			FILE_FIRST    = FILE,
-			FILE_LAST     = FILE,
-			TITLE_FIRST   = TITLE,
-			TITLE_LAST    = TITLE,
-
-			NUMBEROF
-		};
-	private:
-		MType type;
-	public:
-		MessageType(MType type): type(type) {}
-		static const MessageType Message;
-		static const MessageType Notify;
-		static const MessageType Action;
-		static const MessageType Warning;
-		static const MessageType Error;
-		static const MessageType Success;
-		static const MessageType File;
-		static const MessageType Title;
-		bool isMessage() const { return MESSAGE_FIRST <= type && type <= MESSAGE_LAST; }
-		bool isNotify()  const { return NOTIFY_FIRST  <= type && type <= NOTIFY_LAST; }
-		bool isAction()  const { return ACTION_FIRST  <= type && type <= ACTION_LAST; }
-		bool isWarning() const { return WARNING_FIRST <= type && type <= WARNING_LAST; }
-		bool isError()   const { return ERROR_FIRST   <= type && type <= ERROR_LAST; }
-		bool isSuccess() const { return SUCCESS_FIRST <= type && type <= SUCCESS_LAST; }
-		bool isFile()    const { return FILE_FIRST    <= type && type <= FILE_LAST; }
-		bool isTitle()   const { return TITLE_FIRST   <= type && type <= TITLE_LAST; }
-	};
-
-	static const int VERBOSITY_DEFAULT;
-
+class CompilerContext: public MessageFormatter {
 private:
-
-	class MSG {
-	public:
-		MSG(unsigned int id, Location loc, std::string message, MessageType type): id(id), loc(loc), message(message), type(type) {
-		}
-		unsigned int id;
-		Location loc;
-		std::string message;
-		MessageType type;
-		bool operator<(const MSG& other) const {
-			if(loc.getFileName().length()>0 && loc.getFileName() == other.loc.getFileName()) {
-				if(loc.getFirstLine()   < other.loc.getFirstLine()  ) return true;
-				if(loc.getFirstLine()   > other.loc.getFirstLine()  ) return false;
-				if(loc.getFirstColumn() < other.loc.getFirstColumn()) return true;
-				if(loc.getFirstColumn() > other.loc.getFirstColumn()) return false;
-				if(id                   < other.id                  ) return true;
-				if(id                   > other.id                  ) return false;
-				return false;
-			} else {
-				return id < other.id;
-			}
-		}
-		bool operator==(const MSG& other) const {
-			return id == other.id;
-		}
-	};
-
-	ConsoleWriter consoleWriter;
-	unsigned int errors;
-	unsigned int warnings;
 	std::string name;
-	bool m_useColoredMessages;
-	std::set<MSG> messages;
-	int verbosity;
-	
-	void print(const Location& l, const std::string& str, const MessageType& mType);
 public:
 	map<string,string> types;
 	FileContext fileContext[MAX_FILE_NESTING]; // max file nesting of MAX_FILE_NESTING allowed
@@ -138,7 +47,7 @@ public:
 	/**
 	 * Creates a new compiler context with a specific name.
 	 */
-	CompilerContext(std::ostream& out): consoleWriter(out), errors(0), warnings(0), name(""), verbosity(VERBOSITY_DEFAULT) {
+	CompilerContext(std::ostream& out): MessageFormatter(out), name("") {
 	}
 
 	~CompilerContext() {
@@ -189,91 +98,8 @@ public:
 		return fileContext[i];
 	}
 	
-	/**
-	 * Report the specified error string at the specified location.
-	 * The output format is: <file>:<<line>:error:<error>
-	 * @param loc The location (file, line number, etc) where the error
-	 *            originated from.
-	 * @param str The error string to report.
-	 */
-	void reportErrorAt(Location loc, std::string str, const int& verbosityLevel = VERBOSITY_DEFAULT);
+	virtual bool testWritable(std::string fileName);
 
-	/**
-	 * Report the specified warning string at the specified location.
-	 * The output format is: <file>:<line>:warning:<error>
-	 * @param loc The location (file, line number, etc) where the error
-	 *            originated from.
-	 * @param str The error string to report.
-	 */
-	void reportWarningAt(Location loc, std::string str, const int& verbosityLevel = VERBOSITY_DEFAULT);
-
-	/**
-	 * Report the specified error, without a location.
-	 * The output format is: error:<error>
-	 * @param loc The location (file, line number, etc) where the error
-	 *            originated from.
-	 * @param str The error string to report.
-	 */
-	void reportError(std::string str, const int& verbosityLevel = VERBOSITY_DEFAULT);
-
-	/**
-	 * Report the specified error, without a location.
-	 * The output format is: error:<error>
-	 * @param loc The location (file, line number, etc) where the error
-	 *            originated from.
-	 * @param str The error string to report.
-	 */
-	void reportWarning(std::string str, const int& verbosityLevel = VERBOSITY_DEFAULT);
-
-	void reportActionAt(Location loc, std::string, const int& verbosityLevel = VERBOSITY_DEFAULT);
-	void reportAction(std::string, const int& verbosityLevel = VERBOSITY_DEFAULT);
-
-	void reportFile(std::string fileName, std::string contents, const int& verbosityLevel = VERBOSITY_DEFAULT);
-
-	void reportSuccess(std::string  str, const int& verbosityLevel = VERBOSITY_DEFAULT);
-
-	/**
-	 * Returns the number of reported errors.
-	 * @return The number of reported errors.
-	 */
-	unsigned int getErrors() {
-		return errors;
-	}
-
-	/**
-	 * Returns the number of reported warnings.
-	 * @return The number of reported warnings.
-	 */
-	unsigned int getWarnings() {
-		return warnings;
-	}
-	
-	void notify(std::string str, const int& verbosityLevel = VERBOSITY_DEFAULT);
-
-	void message(std::string str, const int& verbosityLevel = VERBOSITY_DEFAULT);
-	
-	void message(std::string str, const MessageType& mType, const int& verbosityLevel = VERBOSITY_DEFAULT);
-	
-	void messageAt(Location loc, std::string str, const MessageType& mType, const int& verbosityLevel = VERBOSITY_DEFAULT);
-	
-	void flush();
-	
-	bool testWritable(std::string fileName);
-
-	void useColoredMessages(bool useColoredMessages) {
-		m_useColoredMessages = useColoredMessages;
-	}
-	const bool& usingColoredMessaged() const { return m_useColoredMessages; }
-
-	void reportErrors(const int& verbosityLevel = VERBOSITY_DEFAULT);
-
-	const int& getVerbosity() const {
-		return verbosity;
-	}
-	
-	void setVerbosity(const int& verbosity) {
-		this->verbosity = verbosity;
-	}
 };
 
 #endif // COMPILER_H
