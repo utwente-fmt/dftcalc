dbdfc5fe6d2ce91f9d9b7a8187ad5f9b627a71c2
Freark van der Berg <fivanderberg@gmail.com>
2012-02-13 10:55:45 +0100

    Added nearest git version to compile time information

--- compiletime.cmake
+++ compiletime.cmake
@@ -10,6 +10,16 @@ execute_process(
 	COMMAND git diff --exit-code > /dev/null
 	RESULT_VARIABLE git_changed OUTPUT_STRIP_TRAILING_WHITESPACE
 )
+set(git_version "*.*.*")
+execute_process(
+	COMMAND git log --tags=*[.]*[.]* --pretty=format:%d -n1
+	COMMAND grep -m1 -oe "[[:digit:]][.][[:digit:]][.][[:digit:]]"
+	OUTPUT_VARIABLE git_version OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+execute_process(
+	COMMAND git branch
+	OUTPUT_VARIABLE git_test OUTPUT_STRIP_TRAILING_WHITESPACE
+)
 file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/compiletime.h
 "/**
  * Generated at compile time on ${date_output}
@@ -17,4 +27,5 @@ file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/compiletime.h
 #define COMPILETIME_DATE \"${date_output}\"
 #define COMPILETIME_GITREV \"${git_output}\"
 #define COMPILETIME_GITCHANGED \"${git_changed}\"
+#define COMPILETIME_GITVERSION \"${git_version}\"
 ")
