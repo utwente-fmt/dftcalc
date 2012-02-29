463f6c33b24637c7be590c71708593252a148d0d
Freark van der Berg <fivanderberg@gmail.com>
2012-02-29 21:12:16 +0100

    Added CADP reader methods for SVL memtime info and BCG info

---
+++ dft2lnt/CADP.cpp
@@ -0,0 +1,19 @@
+#include "CADP.h"
+
+const YAML::Node& operator>>(const YAML::Node& node, DFT::CADP::BCGInfo& bcgInfo) {
+	if(const YAML::Node* itemNode = node.FindValue("states")) {
+		*itemNode >> bcgInfo.states;
+	}
+	if(const YAML::Node* itemNode = node.FindValue("transitions")) {
+		*itemNode >> bcgInfo.transitions;
+	}
+	return node;
+}
+
+YAML::Emitter& operator<<(YAML::Emitter& out, const DFT::CADP::BCGInfo& bcgInfo) {
+	out << YAML::BeginMap;
+	out << YAML::Key << "states"  << YAML::Value << bcgInfo.states;
+	out << YAML::Key << "transitions"  << YAML::Value << bcgInfo.transitions;
+	out << YAML::EndMap;
+	return out;
+}
---
+++ dft2lnt/CADP.h
@@ -0,0 +1,85 @@
+#ifndef CADP_H
+#define CADP_H
+
+#include <fstream>
+
+#include "Shell.h"
+#include "yaml-cpp/yaml.h"
+
+namespace DFT {
+
+class CADP {
+public:
+	
+	class BCGInfo {
+	public:
+		int states;
+		int transitions;
+		BCGInfo():
+			states(0),
+			transitions(0) {
+		}
+	};
+	
+	static bool readStatsFromSVLLog(File logFile, Shell::RunStatistics& stats) {
+		if(!FileSystem::exists(logFile)) {
+			return true;
+		}
+		
+		std::ifstream log(logFile.getFileRealPath());
+		if(!log.is_open()) {
+			return true;
+		}
+		
+		char buffer[200];
+		Shell::RunStatistics statsTemp;
+		while(log.getline(buffer,200)) {
+			if(!Shell::readMemtimeStatistics(buffer,statsTemp)) {
+				stats.addTimeMaxMem(statsTemp);
+			}
+		}
+		return false;
+	}
+	
+	static bool BCG_Info(File bcgFile, BCGInfo& bcgInfo) {
+		if(!FileSystem::exists(bcgFile)) {
+			return true;
+		}
+		
+		File statFile;
+		do {
+			char buffer[L_tmpnam];
+			tmpnam(buffer);
+			statFile = File(string(buffer));
+		} while(FileSystem::exists(statFile));
+
+		Shell::system("bcg_info " + bcgFile.getFileRealPath(),".",statFile.getFileRealPath(),"",5,NULL);
+		
+		std::ifstream log(statFile.getFileRealPath());
+		if(!log.is_open()) {
+			return true;
+		}
+		
+		char buffer[300];
+		while(log.getline(buffer,300)) {
+			if(buffer[0]=='\t') {
+				const char* c = buffer+1;
+				while(*c && isdigit(*c)) c++;
+				if(!strncmp(" states",c,7)) {
+					sscanf(buffer,"	%i",&bcgInfo.states);
+				} else if(!strncmp(" transitions",c,12)) {
+					sscanf(buffer,"	%i",&bcgInfo.transitions);
+				}
+			}
+		}
+		
+		return false;
+	}
+};
+
+} // Namespace: DFT
+
+const YAML::Node& operator>>(const YAML::Node& node, DFT::CADP::BCGInfo& bcgInfo);
+YAML::Emitter& operator<<(YAML::Emitter& out, const DFT::CADP::BCGInfo& bcgInfo);
+
+#endif
