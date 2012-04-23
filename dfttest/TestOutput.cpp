648e3c9aceda562a029e0f065807774c8223f083
Freark van der Berg <fivanderberg@gmail.com>
2012-04-23 09:11:11 +0200

    dfttest: enhanced output, added LaTeX output profile

--- dfttest/TestOutput.cpp
+++ dfttest/TestOutput.cpp
@@ -1,5 +1,11 @@
 #include "TestOutput.h"
 
+void Test::OutputFormatterNice::displaySuiteStart(TestRun* testRun) {
+}
+
+void Test::OutputFormatterNice::displaySuiteEnd(TestRun* testRun) {
+}
+
 void Test::OutputFormatterNice::displayTestStart(TestRun* testRun, TestSpecification* test) {
 	ConsoleWriter& consoleWriter = testRun->messageFormatter->getConsoleWriter();
 	
@@ -19,7 +25,11 @@ void Test::OutputFormatterNice::displayTestStart(TestRun* testRun, TestSpecifica
 	for(const TestResultColumn& column: testRun->reportColumns) {
 		consoleWriter << ConsoleWriter::Color::WhiteBright;
 		consoleWriter.outlineLeftNext(12,' ');
-		consoleWriter << ConsoleWriter::_push << column.name << ConsoleWriter::_pop;
+		consoleWriter << ConsoleWriter::_push << column.name;
+		if(!column.unit.empty()) {
+			consoleWriter << " (" << column.unit << ")";
+		}
+		consoleWriter << ConsoleWriter::_pop;
 		consoleWriter << ConsoleWriter::Color::Reset << " ¦ ";
 	}
 		consoleWriter.appendPostfix();
@@ -116,4 +126,65 @@ void Test::OutputFormatterNice::displayTestResult(TestRun* testRun, TestSpecific
 		consoleWriter << ConsoleWriter::Color::Reset << " ¦ ";
 	}
 	consoleWriter.appendPostfix();
+}
+
+void Test::OutputFormatterLaTeX::displaySuiteStart(TestRun* testRun) {
+	ConsoleWriter& consoleWriter = testRun->messageFormatter->getConsoleWriter();
+	
+		consoleWriter << "& ";
+		consoleWriter.outlineLeftNext(12,' ');
+		consoleWriter << ConsoleWriter::_push << "Iteration" << ConsoleWriter::_pop;
+	for(const TestResultColumn& column: testRun->reportColumns) {
+		consoleWriter << " & ";
+		consoleWriter.outlineLeftNext(12,' ');
+		consoleWriter << ConsoleWriter::_push << escapeUnderscore(column.name);
+		if(!column.unit.empty()) {
+			consoleWriter << " (" << escapeUnderscore(column.unit) << ")";
+		}
+		consoleWriter << ConsoleWriter::_pop;
+	}
+	consoleWriter << " \\\\" << consoleWriter.applypostfix;
+	consoleWriter << "\\testbetween" << consoleWriter.applypostfix;
+}
+
+void Test::OutputFormatterLaTeX::displaySuiteEnd(TestRun* testRun) {
+}
+
+void Test::OutputFormatterLaTeX::displayTestStart(TestRun* testRun, TestSpecification* test) {
+	ConsoleWriter& consoleWriter = testRun->messageFormatter->getConsoleWriter();
+	
+	consoleWriter << "\\testbetween" << consoleWriter.applypostfix;
+	
+	//
+	consoleWriter << "\\testname{" << escapeUnderscore(test->getShortName()) << "}" << consoleWriter.applypostfix;
+	
+	for(const std::pair<std::string,TestResult*>& vtest: test->getVerifiedResults()) {
+		displayTestResult(testRun,test,"",vtest.first,vtest.second,false,Test::VERIFIEDOK);
+	}
+}
+void Test::OutputFormatterLaTeX::displayTestEnd(TestRun* testRun, TestSpecification* test) {
+	ConsoleWriter& consoleWriter = testRun->messageFormatter->getConsoleWriter();
+	
+}
+
+void Test::OutputFormatterLaTeX::displayTestResult(TestRun* testRun, TestSpecification* test, const string& timeStamp, const string& iteration, TestResult* result, bool cached, const Test::ResultStatus& resultStatus) {
+	std::map<std::string,std::string> content;
+	std::map<std::string,ConsoleWriter::Color> colors;
+	if(result) {
+		testRun->fillDisplayMapBase(test,timeStamp,iteration,result,cached,content,colors);
+	}
+	
+	ConsoleWriter& consoleWriter = testRun->messageFormatter->getConsoleWriter();
+	
+	{
+		consoleWriter << "& ";
+		consoleWriter << "\\testcoliteration{" << escapeUnderscore(iteration) << "}";
+	}
+	
+	for(const TestResultColumn& column: testRun->reportColumns) {
+		consoleWriter << " & ";
+		consoleWriter << "\\testcol" << escapeUnderscore(column.id) << "{" << escapeUnderscore(content[column.id]) << "}";
+	}
+	consoleWriter << " \\\\" << consoleWriter.applypostfix;
+	consoleWriter << "\\testbetween" << consoleWriter.applypostfix;
 }
--- dfttest/TestOutput.h
+++ dfttest/TestOutput.h
@@ -23,15 +23,36 @@ enum OutputMode {
 
 class OutputFormatter {
 public:
+	virtual void displaySuiteStart(TestRun* testRun) = 0;
+	virtual void displaySuiteEnd(TestRun* testRun) = 0;
 	virtual void displayTestStart(TestRun* testRun, TestSpecification* test) = 0;
 	virtual void displayTestEnd(TestRun* testRun, TestSpecification* test) = 0;
 	virtual void displayTestResult(TestRun* testRun, TestSpecification* test, const string& timeStamp, const string& iteration, TestResult* result, bool cached, const ResultStatus& resultStatus) = 0;
 };
 
 class OutputFormatterNice: public OutputFormatter {
-	void displayTestStart(TestRun* testRun, TestSpecification* test);
-	void displayTestEnd(TestRun* testRun, TestSpecification* test);
-	void displayTestResult(TestRun* testRun, TestSpecification* test, const string& timeStamp, const string& iteration, TestResult* result, bool cached, const ResultStatus& resultStatus);
+	virtual void displaySuiteStart(TestRun* testRun);
+	virtual void displaySuiteEnd(TestRun* testRun);
+	virtual void displayTestStart(TestRun* testRun, TestSpecification* test);
+	virtual void displayTestEnd(TestRun* testRun, TestSpecification* test);
+	virtual void displayTestResult(TestRun* testRun, TestSpecification* test, const string& timeStamp, const string& iteration, TestResult* result, bool cached, const ResultStatus& resultStatus);
+};
+
+class OutputFormatterLaTeX: public OutputFormatter {
+	virtual void displaySuiteStart(TestRun* testRun);
+	virtual void displaySuiteEnd(TestRun* testRun);
+	virtual void displayTestStart(TestRun* testRun, TestSpecification* test);
+	virtual void displayTestEnd(TestRun* testRun, TestSpecification* test);
+	virtual void displayTestResult(TestRun* testRun, TestSpecification* test, const string& timeStamp, const string& iteration, TestResult* result, bool cached, const ResultStatus& resultStatus);
+	std::string escapeUnderscore(const std::string& str) {
+		std::string n;
+		n.reserve(str.length()+1);
+		for(char c: str) {
+			if(c=='_') n += '\\';
+			n += c;
+		}
+		return n;
+	}
 };
 
 } // Namespace: Test
