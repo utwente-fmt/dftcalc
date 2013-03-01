00b3b6f69cf6ad34b189d1cc11fdebeac9d32e00
Axel Belinfante <Axel.Belinfante@cs.utwente.nl>
2013-03-01 16:19:18 +0100

    try to fix time monotonic vs monotonic_raw using ifdefs

--- dft2lnt/System.cpp
+++ dft2lnt/System.cpp
@@ -32,6 +32,9 @@ void System::Timer::reset() {
 #if SYSTEM_TIMER_BACKEND == SYSTEM_TIMER_BACKEND_MONOTONIC_RAW
 	clock_gettime(CLOCK_MONOTONIC_RAW, &start);
 #endif
+#if SYSTEM_TIMER_BACKEND == SYSTEM_TIMER_BACKEND_MONOTONIC
+	clock_gettime(CLOCK_MONOTONIC, &start);
+#endif
 }
 
 double System::Timer::getElapsedSeconds() {
@@ -49,6 +52,12 @@ double System::Timer::getElapsedSeconds() {
 #if SYSTEM_TIMER_BACKEND == SYSTEM_TIMER_BACKEND_MONOTONIC_RAW
 	timespec now;
 	clock_gettime(CLOCK_MONOTONIC_RAW, &now);
+	return (double)(now.tv_sec -start.tv_sec )
+		 + (double)(now.tv_nsec-start.tv_nsec)*0.000000001;
+#endif
+#if SYSTEM_TIMER_BACKEND == SYSTEM_TIMER_BACKEND_MONOTONIC
+	timespec now;
+	clock_gettime(CLOCK_MONOTONIC, &now);
 	return (double)(now.tv_sec -start.tv_sec )
 		 + (double)(now.tv_nsec-start.tv_nsec)*0.000000001;
 #endif
