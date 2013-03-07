1a99688888d9c77d62e7664a48fd011c3a2a7cff
Axel Belinfante <Axel.Belinfante@cs.utwente.nl>
2013-03-07 10:00:54 +0100

    removed dead code from imca.cpp

--- dft2lnt/imca.cpp
+++ dft2lnt/imca.cpp
@@ -119,49 +119,6 @@ int IMCA::FileHandler::readOutputFile(const File& file) {
 			}
 		}
 	}
-#if 0
-	const char* resultString = NULL;
-	{
-		const char* c = buffer;
-		while(*c) {
-			//printf("%c",*c);
-			if(!strncmp("probability",c,11)) {
-				c += 13;
-				const char* ce = c;
-				while(*ce && *ce!='\n') ce++;
-				resultString = c;
-				//printf("\nfound: %s\n",c);
-				//printf("should be: '%s'\n",resultString.c_str());
-				break;
-			}
-			c++;
-		}
-	}
-
-	if(!resultString) {
-		return 1;
-	}
-
-	{
-		results.clear();
-		const char* c = resultString;
-		while(*c && *c!='\n') {
-			//float res = atof(c);
-			double res = 0;
-			sscanf(c,"%lf",&res);
-			results.push_back(std::pair<std::string,IMCA::T_Chance>("?",res));
-			//printf("found result: %f\n",res);
-
-			const char* ce = c;
-			while(*ce && *ce!=' ') ce++;
-			if(!*ce) break;
-			c = ce+1;
-		}
-
-		i_isCalculated = true;
-	}
-#endif
-
 
 	free(buffer);
 	return 0;
