309875c1f192d0d2fe132a9d3b678d3b3fa610e9
Axel Belinfante <Axel.Belinfante@cs.utwente.nl>
2013-03-07 09:55:43 +0100

    extend imca output parser for expected time, and for timebound line prefixes
    Now recognize lines that contain "Maximal expected time"
    Now recognize a 'tb=nr ' prefix on lines that have "probability:"

--- dft2lnt/imca.cpp
+++ dft2lnt/imca.cpp
@@ -38,28 +38,85 @@ int IMCA::FileHandler::readOutputFile(const File& file) {
 	n = fread(buffer,len,1,fp); //read into buffer
 	fclose(fp);
 
-	fprintf(stdout, "imca buffer info (%ld,%ld):\n", len, n);
-	fprintf(stdout, "imca buffer\n%*s\n", (int)n, buffer);
+	//fprintf(stdout, "imca buffer info (%ld,%ld):\n", len, n);
+	//fprintf(stdout, "imca buffer\n%*s\n", (int)n, buffer);
 
 	// for each line:
 	char *p = buffer;
 	char *k;
-	char needle[] = "probability: ";
-	while((k= strstr(p, needle)) != 0) {
-		char *prob = k + strlen(needle);
-		char *e = strchr(prob, '\n');
-		if (e != 0)
-			*e = '\0';
-		double res = 0;
-                int r  = sscanf(prob,"%lf",&res);
-		if (r ==  1){
-			results.push_back(std::pair<std::string,IMCA::T_Chance>("?",res));
+	char tb_needle[] = "tb=";
+	char prob_needle[] = "probability: ";
+	char et_needle[] = "Maximal expected time: ";
+	if((k=strstr(p, et_needle)) != 0) {
+		char *et = k + strlen(et_needle);
+		char *et_e = strchr(et, '\n');
+		if (et_e != 0) {
+			*et_e = '\0';
+			p = et_e + 1;
+		} else {
+			p = et;
+		}
+		double et_res = 0;
+                int r_et  = sscanf(et,"%lf",&et_res);
+		if (r_et ==  1){
+			results.push_back(std::pair<std::string,IMCA::T_Chance>("?",et_res));
 			i_isCalculated = true;
 		}
-		if (e != 0)
-			p = e + 1;
-		else
-			p = prob;
+	} else {
+		p = buffer;
+		while((k= strstr(p, prob_needle)) != 0) {
+			// find start of line, by looking for end of previous line (either '\n' or '\0')
+			char *ep = k;
+			while(ep > p && *ep != '\0' && *ep != '\n')
+				ep--;
+			char *line_start;
+			if (*ep == '\0' || *ep == '\n')
+				line_start = ep + 1;
+			else
+				line_start = p;
+			//fprintf(stderr, "prob found, p=%p, k=%p, ep=%p, line_start=%p\n", p, k, ep, line_start);
+
+			// get probability value, turning end-of-line into \0
+			int r_prob = 0;
+			double prob_res = 0;
+			char *prob = k + strlen(prob_needle);
+			char *prob_e = strchr(prob, '\n');
+			if (prob_e != 0) {
+				*prob_e = '\0';
+				p = prob_e + 1;
+			} else {
+				p = prob;
+			}
+                	r_prob  = sscanf(prob,"%lf",&prob_res);
+
+			// look for tb= value, from start of line
+			// we will not cross into next line, because end-of-line is turned into \0
+			int r_tb = 0;
+			double tb_res = 0;
+			char *tb;
+			if ((k = strstr(line_start, tb_needle)) != 0) {
+				//fprintf(stderr, "tb found, p=%p, k=%p, ep=%p, line_start=%p\n", p, k, ep, line_start);
+				tb = k + strlen(tb_needle);
+				char *tb_e = strchr(tb, ' ');
+				if (tb_e != 0) {
+					*tb_e = '\0';
+					//p = tb_e + 1;
+				} else {
+					//p = tb;
+				}
+                		r_tb  = sscanf(tb,"%lf",&tb_res);
+			}
+			//fprintf(stderr, "r_prob=%d r_tb=%d\n", r_prob, r_tb);
+
+			if (r_tb ==  1 && r_prob == 1){
+				string tb_str(tb);
+				results.push_back(std::pair<std::string,IMCA::T_Chance>(tb_str,prob_res));
+				i_isCalculated = true;
+			} else if (r_prob == 1){
+				results.push_back(std::pair<std::string,IMCA::T_Chance>("?",prob_res));
+				i_isCalculated = true;
+			}
+		}
 	}
 #if 0
 	const char* resultString = NULL;
