fc26d38e33238678bf3466099de077e8b7c38901
Freark van der Berg <f.i.vanderberg@student.utwente.nl>
2013-09-18 15:33:18 +0200

    Added option to specify GIT information on CMake command line

--- compiletime.cmake
+++ compiletime.cmake
@@ -2,25 +2,28 @@ execute_process(
 	COMMAND date "+%Y-%m-%d %H:%M:%S"
 	OUTPUT_VARIABLE date_output OUTPUT_STRIP_TRAILING_WHITESPACE
 )
-execute_process(
-	COMMAND git rev-parse --verify HEAD
-	OUTPUT_VARIABLE git_output OUTPUT_STRIP_TRAILING_WHITESPACE
-)
-execute_process(
-	COMMAND git diff --exit-code
-	OUTPUT_QUIET
-	ERROR_QUIET
-	RESULT_VARIABLE git_changed
-)
-set(git_version "*.*.*")
-execute_process(
-	COMMAND git describe --tags --match=v[0-9]* --abbrev=0
-	OUTPUT_VARIABLE git_version OUTPUT_STRIP_TRAILING_WHITESPACE
-)
-execute_process(
-	COMMAND git branch
-	OUTPUT_VARIABLE git_test OUTPUT_STRIP_TRAILING_WHITESPACE
-)
+if(USECOMMANDLINEGITINFO STREQUAL "YES")
+	execute_process(
+		COMMAND git rev-parse --verify HEAD
+		OUTPUT_VARIABLE git_output OUTPUT_STRIP_TRAILING_WHITESPACE
+	)
+	execute_process(
+		COMMAND git diff --exit-code
+		OUTPUT_QUIET
+		ERROR_QUIET
+		RESULT_VARIABLE git_changed
+	)
+	set(git_version "*.*.*")
+	execute_process(
+		COMMAND git describe --tags --match=v[0-9]* --abbrev=0
+		OUTPUT_VARIABLE git_version OUTPUT_STRIP_TRAILING_WHITESPACE
+	)
+	execute_process(
+		COMMAND git branch
+		OUTPUT_VARIABLE git_test OUTPUT_STRIP_TRAILING_WHITESPACE
+	)
+endif()
+
 file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/compiletime.h
 "/**
  * Generated at compile time on ${date_output}
