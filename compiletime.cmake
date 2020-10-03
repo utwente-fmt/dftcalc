d2453c29c25b52ef9def2d1b7d019f81e18c378c
Enno Ruijters <mail@ennoruijters.nl>
2020-10-03 15:16:22 +0200

    Support compilation without git present

--- compiletime.cmake
+++ compiletime.cmake
@@ -1,38 +1,47 @@
-if(USECOMMANDLINEGITINFO STREQUAL "YES")
+if (${NO_GIT})
+	set(GIT "GIT-NOTFOUND")
+else()
+	find_program(GIT git)
+endif()
+
+if (${GIT} STREQUAL "GIT-NOTFOUND")
+	set(git_output "UNKNOWN")
+	set(git_changed 0)
+	set(git_version "v${FALLBACK_VERSION}?")
 else()
 	execute_process(
-		COMMAND git rev-parse --verify HEAD
+		COMMAND ${GIT} rev-parse --verify HEAD
 		OUTPUT_VARIABLE git_output OUTPUT_STRIP_TRAILING_WHITESPACE
 	)
 	execute_process(
-		COMMAND git describe HEAD
+		COMMAND ${GIT} describe HEAD
 		OUTPUT_VARIABLE git_descr OUTPUT_STRIP_TRAILING_WHITESPACE
 	)
 	execute_process(
-		COMMAND git diff --exit-code
+		COMMAND ${GIT} diff --exit-code
 		OUTPUT_QUIET
 		ERROR_QUIET
 		RESULT_VARIABLE git_changed
 	)
 	set(git_version "*.*.*")
 	execute_process(
-		COMMAND git describe --tags --match=v[0-9]* --abbrev=0
+		COMMAND ${GIT} describe --tags --match=v[0-9]* --abbrev=0
 		OUTPUT_VARIABLE git_version OUTPUT_STRIP_TRAILING_WHITESPACE
 	)
 	execute_process(
-		COMMAND git branch
+		COMMAND ${GIT} branch
 		OUTPUT_VARIABLE git_test OUTPUT_STRIP_TRAILING_WHITESPACE
 	)
-endif()
 
-if (git_version STREQUAL git_descr)
-else()
-	set(git_version ${git_version}+)
+	if (git_version STREQUAL git_descr)
+	else()
+		set(git_version ${git_version}+)
+	endif()
 endif()
 
 string(TIMESTAMP date "%Y-%m-%d %H:%M:%S")
 
-file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/compiletime.h
+file(WRITE ${OUTPUT_FILE}
 "/**
  * Generated at compile time on ${date}
  */
